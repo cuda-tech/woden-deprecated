365a00cf362ada01bd5f3f1cac27b382179205f1
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2020-04-02 22:42:22 +0800

    添加 mysql 注解

--- src/main/kotlin/datahub/models/Task.kt
+++ src/main/kotlin/datahub/models/Task.kt
@@ -13,6 +13,7 @@
  */
 package datahub.models
 
+import datahub.models.annotations.*
 import datahub.models.dtype.SchedulePeriod
 import me.liuwj.ktorm.entity.Entity
 import java.time.LocalDateTime
@@ -24,24 +25,82 @@ import java.time.LocalDateTime
 interface Task : Entity<Task> {
     companion object : Entity.Factory<Task>()
 
+    @BIGINT
+    @COMMENT("任务 ID")
+    @PRIMARY_KEY
+    @AUTO_INCREMENT
     val id: Int
+
+    @BIGINT
+    @COMMENT("镜像 ID")
     var mirrorId: Int
+
+    @VARCHAR(512)
+    @COMMENT("任务名")
     var name: String
+
+    @JSON
+    @COMMENT("负责人")
     var owners: Set<Int>
+
+    @TEXT
+    @COMMENT("执行参数")
     var args: String
+
+    @BOOL
+    @COMMENT("执行失败是否跳过")
     var softFail: Boolean
+
+    @VARCHAR(10)
+    @COMMENT("调度周期")
     var period: SchedulePeriod
+
+    @VARCHAR(32)
+    @COMMENT("执行队列")
     var queue: String
+
+    @SMALLINT
+    @COMMENT("优先级")
     var priority: Int
+
+    @INT
+    @COMMENT("最大等待时间（分钟）")
     var pendingTimeout: Int
+
+    @INT
+    @COMMENT("最大执行时间（分钟）")
     var runningTimeout: Int
+
+    @JSON
+    @COMMENT("父任务列表")
     var parent: Map<Int, Map<String, String>>
+
+    @JSON
+    @COMMENT("子任务列表")
     var children: Map<Int, Map<String, String>>
+
+    @SMALLINT
+    @COMMENT("重试次数")
     var retries: Int
+
+    @INT
+    @COMMENT("重试间隔")
     var retryDelay: Int
+
+    @BOOL
+    @COMMENT("调度是否生效")
     var valid: Boolean
+
+    @BOOL
+    @COMMENT("逻辑删除")
     var isRemove: Boolean
+
+    @DATE_TIME
+    @COMMENT("创建时间")
     var createTime: LocalDateTime
+
+    @DATE_TIME
+    @COMMENT("更新时间")
     var updateTime: LocalDateTime
 }
 
