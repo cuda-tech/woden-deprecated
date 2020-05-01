0fb00d0439b3fc152898826cf805bb4b362bc4f0
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2020-05-02 00:21:52 +0800

    添加 DTO

---
+++ service/src/main/kotlin/tech/cuda/datahub/service/dto/FileContentDTO.kt
@@ -0,0 +1,26 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tech.cuda.datahub.service.dto
+
+import tech.cuda.datahub.annotation.pojo.DTO
+import tech.cuda.datahub.service.po.FilePO
+
+/**
+ * @author Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
+ * @since 1.0.0
+ */
+@DTO(FilePO::class)
+data class FileContentDTO(
+    val content: String?
+)
