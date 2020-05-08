5a8bc5f8f8447d8cbf2ffb3d139204510411a062
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2020-05-09 02:32:03 +0800

    添加 mysql 的 json_contains 函数支持

---
+++ service/src/main/kotlin/tech/cuda/datahub/service/mysql/function/functions.kt
@@ -0,0 +1,36 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tech.cuda.datahub.service.mysql.function
+
+import me.liuwj.ktorm.expression.ArgumentExpression
+import me.liuwj.ktorm.expression.FunctionExpression
+import me.liuwj.ktorm.schema.BooleanSqlType
+import me.liuwj.ktorm.schema.ColumnDeclaring
+import me.liuwj.ktorm.schema.VarcharSqlType
+
+/**
+ * @author Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
+ * @since 1.0.0
+ */
+fun ColumnDeclaring<Set<Int>>.contains(item: Int): FunctionExpression<Boolean> {
+    return FunctionExpression(
+        functionName = "json_contains",
+        arguments = listOf(
+            this.asExpression(),
+            ArgumentExpression(item.toString(), VarcharSqlType),
+            ArgumentExpression("$", VarcharSqlType)
+        ),
+        sqlType = BooleanSqlType
+    )
+}
