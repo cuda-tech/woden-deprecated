0385c5f3589c52512cb74c895079b33633c45e5b
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2020-05-29 00:18:56 +0800

    添加文档

--- frontend/src/components/selections/GroupSelection.vue
+++ frontend/src/components/selections/GroupSelection.vue
@@ -5,36 +5,44 @@
 <template>
     <Select :value="value" :multiple="multiple" @on-change="val => this.$emit('input', val)" placeholder="项目组">
         <Icon custom="iconfont icon-project" slot="prefix"/>
-        <Option v-for="group in groups" :value="group.id">
+        <Option v-for="group in groups" :value="group.id" :key="group.id">
             {{ group.name }}
         </Option>
     </Select>
 </template>
 
 <script>
+    import GroupAPI from '../../api/GroupAPI';
+
     /**
      * 项目组选择菜单
      */
     export default {
-        name: "GroupSelection",
+        name: 'GroupSelection',
         props: {
+            /**
+             * 绑定的项目组 ID, 可使用 v-model 双向绑定
+             */
             value: {
-                default: null,
+                type: Number,
+                default: null
             },
+
+            /**
+             * 是否支持多选
+             */
             multiple: {
                 type: Boolean,
                 default: false
             }
         },
         beforeMount() {
-            this.axios.get("/group").then(data => {
-                this.groupsCount = data.count;
-                this.groups = data.groups;
-            })
+            GroupAPI.listing(1, 999999, null, (count, groups) => {
+                this.groups = groups;
+            });
         },
         data() {
             return {
-                groupsCount: 0,
                 groups: []
             }
         }
