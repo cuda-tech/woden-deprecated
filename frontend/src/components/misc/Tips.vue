a6b27c48c4a3bec7e57ae8c4105906d88b9e198d
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2020-05-30 02:16:38 +0800

    添加 Tips 组件

---
+++ frontend/src/components/misc/Tips.vue
@@ -0,0 +1,33 @@
+<template>
+    <Tooltip :placement="placement">
+        <Icon type="md-help-circle"/>
+        <div slot="content">
+            <slot/>
+        </div>
+    </Tooltip>
+</template>
+<script>
+    /**
+     * 帮助提示
+     */
+    export default {
+        name: 'Tips',
+        props: {
+            /**
+             * 提示信息
+             */
+            message: {
+                type: String,
+                default: ''
+            },
+
+            /**
+             *
+             */
+            placement: {
+                type: String,
+                default: 'top'
+            }
+        },
+    }
+</script>
