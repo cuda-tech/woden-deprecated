6c0017c5c9afc898037f92fadcd3edd0eab8d8a4
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2020-06-05 01:22:01 +0800

    补充文档

--- frontend/src/components/misc/Tips.vue
+++ frontend/src/components/misc/Tips.vue
@@ -2,6 +2,7 @@
     <Tooltip :placement="placement">
         <Icon type="md-help-circle"/>
         <div slot="content">
+            <!-- @slot tips 的内容 -->
             <slot/>
         </div>
     </Tooltip>
@@ -9,20 +10,18 @@
 <script>
     /**
      * 帮助提示
+     * @author Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
+     * @since 1.0.0
      */
     export default {
         name: 'Tips',
         props: {
             /**
-             * 提示信息
-             */
-            message: {
-                type: String,
-                default: ''
-            },
-
-            /**
-             *
+             * 弹出方向, 可选值: <br>
+             * top, top-start, top-end <br>
+             * bottom, bottom-start, bottom-end <br>
+             * left, left-start, left-end <br>
+             * right, right-start, right-end <br>
              */
             placement: {
                 type: String,
