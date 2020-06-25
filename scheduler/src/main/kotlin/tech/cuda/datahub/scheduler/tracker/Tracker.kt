1551a1c60c177e0fd8db917f54e1cb8b11287ef3
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2020-06-26 01:38:18 +0800

    Tracker 基类添加生命周期回调

--- scheduler/src/main/kotlin/tech/cuda/datahub/scheduler/tracker/Tracker.kt
+++ scheduler/src/main/kotlin/tech/cuda/datahub/scheduler/tracker/Tracker.kt
@@ -16,8 +16,8 @@ package tech.cuda.datahub.scheduler.tracker
 import kotlinx.coroutines.*
 import org.apache.log4j.Logger
 import tech.cuda.datahub.scheduler.listener.ClockListener
+import tech.cuda.datahub.scheduler.listener.TrackerLifeCycleListener
 import java.lang.AssertionError
-import java.time.LocalDate
 import java.time.LocalDateTime
 
 /**
@@ -25,16 +25,28 @@ import java.time.LocalDateTime
  * @author Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
  * @since 1.0.0
  */
-abstract class Tracker : ClockListener {
+abstract class Tracker : TrackerLifeCycleListener, ClockListener {
     protected val logger: Logger = Logger.getLogger(this::class.java)
     private val className: String get() = this.javaClass.simpleName
     private val heartbeat = 500L
-    private var started: Boolean = false
     private lateinit var job: Deferred<Unit>
+    private lateinit var datetimeSnapshot: LocalDateTime
 
-    private var datetimeSnapshot = LocalDateTime.of(1970, 1, 1, 0, 0, 0)
-    private fun dateChange() = LocalDate.now().isAfter(datetimeSnapshot.toLocalDate())
-    private fun hourChange() = LocalDate.now().isAfter(datetimeSnapshot.toLocalDate())
+    private fun dealWithHourChange(current: LocalDateTime) {
+        if (current.hour != datetimeSnapshot.hour) {
+            onHourChange()
+            logger.info("hour change")
+        }
+    }
+
+    private fun dealWithDateChange(current: LocalDateTime) {
+        if (current.toLocalDate().isAfter(datetimeSnapshot.toLocalDate())) {
+            onDateChange()
+            logger.info("day change")
+        }
+    }
+
+    private fun dealWithHeartBeat() = onHeartBeat()
 
 
     fun start() {
@@ -43,33 +55,32 @@ abstract class Tracker : ClockListener {
             return
         }
         this.job = GlobalScope.async {
-            while (true) {
-                try {
-                    if (dateChange()) {
-                        onDateChange()
-                        logger.info("day change")
-                    }
-                    if (hourChange()) {
-                        onHourChange()
-                        logger.info("hour change")
-                    }
-                    onHeartBeat()
+            try {
+                onStarted()
+                datetimeSnapshot = LocalDateTime.now()
+                while (true) {
+                    val current = LocalDateTime.now()
+                    dealWithDateChange(current)
+                    dealWithHourChange(current)
+                    dealWithHeartBeat()
                     delay(heartbeat)
-                    datetimeSnapshot = LocalDateTime.now()
+                    datetimeSnapshot = current
                     logger.debug("$className alive")
-                } catch (exception: Throwable) {
-                    when (exception) {
-                        // 不捕获由协程取消引起的 CancellationException 和单测引起的 AssertionError
-                        is CancellationException, is AssertionError -> throw exception
-                        else -> {
-                            logger.error(exception.message)
-                            exception.printStackTrace()
-                        }
+                }
+            } catch (exception: Throwable) {
+                when (exception) {
+                    // 不捕获由协程取消引起的 CancellationException 和单测引起的 AssertionError
+                    is CancellationException, is AssertionError -> throw exception
+                    else -> {
+                        logger.error(exception.message)
+                        exception.printStackTrace()
                     }
                 }
+            } finally {
+                onDestroyed()
             }
         }
-        logger.info("$className started").also { this.started = true }
+        logger.info("$className started")
     }
 
     suspend fun await() {
