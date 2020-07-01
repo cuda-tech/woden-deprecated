c6513bbacb52e12df291e810675d78b33f6140c1
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2020-07-02 00:06:13 +0800

    添加 MachineTracker 实现

--- scheduler/src/main/kotlin/tech/cuda/datahub/scheduler/tracker/MachineTracker.kt
+++ scheduler/src/main/kotlin/tech/cuda/datahub/scheduler/tracker/MachineTracker.kt
@@ -13,10 +13,59 @@
  */
 package tech.cuda.datahub.scheduler.tracker
 
+import tech.cuda.datahub.scheduler.util.MachineUtil
+import tech.cuda.datahub.service.MachineService
+import tech.cuda.datahub.service.dto.MachineDTO
+
 /**
+ * 负责服务器注册 & 负载更新
+ * [afterStarted]: Tracker 启动后的回调
  * @author Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
  * @since 1.0.0
  */
-class MachineTracker {
-// todo
+class MachineTracker(private val afterStarted: (MachineDTO) -> Unit = {}) : Tracker() {
+
+    lateinit var machine: MachineDTO
+
+    /**
+     * 启动时根据 MAC 地址判断机器是否已注册
+     * 如果尚未注册，则注册机器
+     * 如果已注册，则检查 IP 和 hostname 是否发生变更，如果发生变更则更新
+     * 最后更新机器的负载信息
+     */
+    override fun onStarted() {
+        val systemInfo = MachineUtil.systemInfo
+        val loadInfo = MachineUtil.loadInfo
+        machine = MachineService.findByMac(systemInfo.mac)
+            ?: MachineService.create(systemInfo.ip, systemInfo.hostname, systemInfo.mac)
+        machine = MachineService.update(
+            id = machine.id,
+            ip = if (systemInfo.ip != machine.ip) systemInfo.ip else null,
+            hostname = if (systemInfo.hostname != machine.hostname) systemInfo.hostname else null,
+            cpuLoad = loadInfo.cpu,
+            memLoad = loadInfo.memory,
+            diskUsage = loadInfo.diskUsage
+        )
+        afterStarted(machine)
+    }
+
+    override fun onDestroyed() {}
+
+    override fun onDateChange() {}
+
+    override fun onHourChange() {}
+
+    /**
+     * 每次心跳检查机器负载并更新
+     */
+    override fun onHeartBeat() {
+        val loadInfo = MachineUtil.loadInfo
+        machine = MachineService.update(
+            id = machine.id,
+            cpuLoad = loadInfo.cpu,
+            memLoad = loadInfo.memory,
+            diskUsage = loadInfo.diskUsage
+        )
+    }
+
 }
--- scheduler/src/test/kotlin/tech/cuda/datahub/scheduler/tracker/MachineTrackerTest.kt
+++ scheduler/src/test/kotlin/tech/cuda/datahub/scheduler/tracker/MachineTrackerTest.kt
@@ -13,4 +13,69 @@
  */
 package tech.cuda.datahub.scheduler.tracker
 
-class MachineTrackerTest
+import io.kotest.matchers.ints.shouldBeGreaterThan
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.mockk.every
+import io.mockk.mockkObject
+import kotlinx.coroutines.runBlocking
+import tech.cuda.datahub.scheduler.TestWithDistribution
+import tech.cuda.datahub.scheduler.util.MachineUtil
+import tech.cuda.datahub.toLocalDateTime
+
+class MachineTrackerTest : TestWithDistribution("machines") {
+
+    @Test
+    fun testStart() = runBlocking {
+        mockkObject(MachineUtil)
+        every { MachineUtil.systemInfo } returns MachineUtil.SystemInfo("17.212.169.100", "9E-EE-49-FA-00-F4", "nknvleif")
+        val machineTracker = MachineTracker(afterStarted = {
+            it.id shouldBe 3
+            it.ip shouldBe "17.212.169.100"
+            it.mac shouldBe "9E-EE-49-FA-00-F4"
+            it.hostname shouldBe "nknvleif"
+            it.cpuLoad shouldBeGreaterThan 0
+            it.memLoad shouldBeGreaterThan 0
+            it.diskUsage shouldBeGreaterThan 0
+            it.updateTime shouldNotBe "2036-03-31 18:40:59".toLocalDateTime()
+        })
+        machineTracker.start()
+        machineTracker.cancelAndAwait()
+    }
+
+    @Test
+    fun testStartWhenIpAndHostChange() = runBlocking {
+        mockkObject(MachineUtil)
+        every { MachineUtil.systemInfo } returns MachineUtil.SystemInfo("192.168.1.1", "9E-EE-49-FA-00-F4", "HOSTNAME")
+        val machineTracker = MachineTracker(afterStarted = {
+            it.id shouldBe 3
+            it.ip shouldBe "192.168.1.1"
+            it.mac shouldBe "9E-EE-49-FA-00-F4"
+            it.hostname shouldBe "HOSTNAME"
+            it.cpuLoad shouldBeGreaterThan 0
+            it.memLoad shouldBeGreaterThan 0
+            it.diskUsage shouldBeGreaterThan 0
+            it.updateTime shouldNotBe "2036-03-31 18:40:59".toLocalDateTime()
+        })
+        machineTracker.start()
+        machineTracker.cancelAndAwait()
+    }
+
+    @Test
+    fun testStartWhenNotRegister() = runBlocking {
+        mockkObject(MachineUtil)
+        every { MachineUtil.systemInfo } returns MachineUtil.SystemInfo("192.168.1.1", "01-23-45-67-89-AB", "HOSTNAME")
+        val machineTracker = MachineTracker(afterStarted = {
+            it.id shouldBe 247
+            it.ip shouldBe "192.168.1.1"
+            it.mac shouldBe "01-23-45-67-89-AB"
+            it.hostname shouldBe "HOSTNAME"
+            it.cpuLoad shouldBeGreaterThan 0
+            it.memLoad shouldBeGreaterThan 0
+            it.diskUsage shouldBeGreaterThan 0
+        })
+        machineTracker.start()
+        machineTracker.cancelAndAwait()
+    }
+
+}
