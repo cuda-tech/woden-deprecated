45cdfabb607a9e1c873520f18f9b387c46a16a0d
Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
2020-07-05 00:06:54 +0800

    添加 Bash Operator

--- scheduler/src/main/kotlin/tech/cuda/datahub/scheduler/ops/BashOperator.kt
+++ scheduler/src/main/kotlin/tech/cuda/datahub/scheduler/ops/BashOperator.kt
@@ -13,100 +13,10 @@
  */
 package tech.cuda.datahub.scheduler.ops
 
-import org.apache.commons.exec.*
-import org.apache.commons.exec.environment.EnvironmentUtils
-import org.apache.commons.io.IOUtils
-import org.quartz.JobExecutionContext
-import tech.cuda.datahub.service.model.Task
-import java.io.ByteArrayOutputStream
-import java.io.IOException
-
+import tech.cuda.datahub.service.dto.TaskDTO
 
 /**
  * @author Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
  * @since 1.0.0
  */
-class BashOperator(task: Task, command: String = "echo ") : Operator(task) {
-    private val EXIT_VALUE = 0
-    private val commandLine: CommandLine = CommandLine("bash")
-    private val std: ByteArrayOutputStream
-    private val handle: DefaultExecuteResultHandler
-    private val killer: ExecuteWatchdog
-    private val executor: DefaultExecutor
-
-
-    init {
-        commandLine.addArgument("-c")
-        commandLine.addArgument(command, false)
-        this.std = ByteArrayOutputStream()
-        this.handle = DefaultExecuteResultHandler()
-        this.killer = ExecuteWatchdog(Long.MAX_VALUE)
-        this.executor = DefaultExecutor()
-        this.executor.setExitValue(EXIT_VALUE)
-        this.executor.streamHandler = PumpStreamHandler(std, std)
-        this.executor.watchdog = killer
-    }
-
-    val output: String
-        get() {
-            return this.std.toString()
-        }
-
-    val isFinish: Boolean
-        get() {
-            return this.handle.hasResult() || this.killer.killedProcess()
-        }
-
-    val isSuccess: Boolean
-        get() {
-            return try {
-                this.isFinish && this.handle.exitValue == EXIT_VALUE
-            } catch (e: IllegalStateException) {
-                false
-            }
-        }
-
-    fun kill() {
-        try {
-            // we need to get the watch dog process first
-            var field = ExecuteWatchdog::class.java.getDeclaredField("process")
-            field.isAccessible = true
-            val process = field.get(this.killer)
-
-            // then get the pid of process
-            field = process.javaClass.getDeclaredField("pid")
-            field.isAccessible = true
-            val pid = field.getInt(process)
-
-            // finally, we get the process tree of pid and kill each of them
-            val pstree = Runtime.getRuntime().exec("pstree -p $pid")
-            pstree.waitFor()
-
-            """(?<=\()[^)]+""".toRegex().findAll(IOUtils.toString(pstree.inputStream)).forEach {
-                val kill = Runtime.getRuntime().exec("kill -9 ${it.value}")
-                kill.waitFor()
-            }
-
-        } catch (e: IllegalAccessException) {
-            logger.error("get watch dog process failed")
-        } catch (e: InterruptedException) {
-            logger.error("kill pstree failed")
-        } catch (e: NoSuchFieldException) {
-            logger.error("get pid field from watch dog failed")
-        } catch (e: IOException) {
-            logger.error("parse pstree result failed")
-        }
-    }
-
-
-    override fun process(context: JobExecutionContext?) {
-        try {
-            executor.execute(commandLine, EnvironmentUtils.getProcEnvironment(), this.handle)
-        } catch (e: IOException) {
-            this.executor.setExitValue(-1)
-            // todo: 日志打印
-        }
-        this.handle.waitFor()
-    }
-
-}
+class BashOperator(task: TaskDTO, type: String = "bash") : BashBaseOperator(task, type)
--- scheduler/src/test/kotlin/tech/cuda/datahub/scheduler/ops/BashOperatorTest.kt
+++ scheduler/src/test/kotlin/tech/cuda/datahub/scheduler/ops/BashOperatorTest.kt
@@ -13,4 +13,67 @@
  */
 package tech.cuda.datahub.scheduler.ops
 
-class BashOperatorTest
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import tech.cuda.datahub.scheduler.TestWithDistribution
+import tech.cuda.datahub.service.TaskService
+
+/**
+ * @author Jensen Qi <jinxiu.qi@alu.hit.edu.cn>
+ * @since 1.0.0
+ */
+class BashOperatorTest : TestWithDistribution("tasks", "file_mirrors"), TempFileGetter {
+
+    @Test
+    fun success() {
+        val bash = BashOperator(TaskService.findById(3)!!) // mirror_id = 143
+        bash.start()
+        val tempFile = Thread.sleep(100).run { getTempFile("bash_") }
+        tempFile.exists() shouldBe true
+        bash.isFinish shouldBe false
+        bash.isSuccess shouldBe false
+        bash.output shouldBe ""
+
+        var bufferOutput = ""
+        while (!bash.isFinish) {
+            bufferOutput = bash.output
+            Thread.sleep(500)
+        }
+        bufferOutput shouldBe "hello\n"
+
+        bash.join()
+        bash.isFinish shouldBe true
+        bash.isSuccess shouldBe true
+        bash.output shouldBe "hello\nworld\n"
+        tempFile.exists() shouldBe false
+    }
+
+    @Test
+    fun failed() {
+        val bash = BashOperator(TaskService.findById(8)!!) // mirror_id = 190
+        bash.start()
+        val tempFile = Thread.sleep(100).run { getTempFile("bash_") }
+        tempFile.exists() shouldBe true
+        bash.isFinish shouldBe false
+        bash.isSuccess shouldBe false
+        bash.join()
+        bash.output shouldContain "line 1: command_not_exists: command not found"
+        bash.isFinish shouldBe true
+        bash.isSuccess shouldBe false
+        tempFile.exists() shouldBe false
+    }
+
+    @Test
+    fun kill() {
+        val bash = BashOperator(TaskService.findById(4)!!) // mirror_id = 58
+        bash.start()
+        val tempFile = Thread.sleep(100).run { getTempFile("bash_") }
+        tempFile.exists() shouldBe true
+        bash.kill()
+        bash.join()
+        bash.isFinish shouldBe true
+        bash.isSuccess shouldBe false
+        tempFile.exists() shouldBe false
+    }
+
+}
